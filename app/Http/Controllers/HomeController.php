e3332ddd482b3bb934540b5ad84d33c691b1265d
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2021-02-12 17:11:20 +0100

    try catch nel delete

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\File;
+
 
 use App\Mail\TestMail;
 
@@ -51,6 +53,8 @@ class HomeController extends Controller
             'icon' => 'required|file'
         ]);
 
+        $this -> deleteUserIcon();
+
         $image = $request -> file('icon');
 
         $ext = $image -> getClientOriginalExtension();
@@ -70,12 +74,45 @@ class HomeController extends Controller
 
     public function clearUserIcon() {
 
+        $this -> deleteUserIcon();
+
         $user = Auth::user();
         $user -> icon = null;
         $user -> save();
 
         return redirect() -> back();
 
+
+    }
+
+    private function deleteUserIcon() {
+
+
+
+        $user = Auth::user();
+
+        /*
+        if ($user -> icon) {
+
+            $fileName = $user -> icon;
+
+            $file = storage_path('app/public/icon/' . $fileName);
+            File::delete($file);
+    
+
+        }
+        */
+
+        //OPPURE MEGLIO FARE
+
+        try {
+
+            $fileName = $user -> icon;
+
+            $file = storage_path('app/public/icon/' . $fileName);
+            $res = File::delete($file);
+
+        } catch(\Exception $e) { }
 
     }
 
